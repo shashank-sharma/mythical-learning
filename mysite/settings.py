f98a882a60b0a961838e8dfae2f2f1b6a7b405fb
shashank <shashank.sharma98@gmail.com>
2017-05-14 16:43:03 +0530

    CSS, JS, Image, Sample website added

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Additional information
 DATABASES['default'] = dj_database_url.config()
