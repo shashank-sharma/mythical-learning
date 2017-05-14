d5f46ea1079416090c5b33c2d0a6fd539e7bc2ee
shashank <shashank.sharma98@gmail.com>
2017-05-14 11:13:19 +0530

    Additional files and changes for Heroku

--- .gitignore
+++ .gitignore
@@ -1,7 +1,5 @@
 *.pyc
-*~
-__pycache__
-myvenv
 db.sqlite3
-/static
-.DS_Store
+myvenv
+__pycache__
+local_settings.py
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,3 +119,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional information
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
