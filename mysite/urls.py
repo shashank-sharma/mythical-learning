4cc3328d3aea660b3bb5dec90f6b1910dd3d3284
shashank <shashank.sharma98@gmail.com>
2017-05-17 22:45:15 +0530

    #1 Homepage Created + Images

--- mysite/urls.py
+++ mysite/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         login_views.activate, name='activate'),
     url(r'^$', login_views.home, name='home'),
-    url(r'', include('home.urls')),
+    url(r'w/', include('home.urls')),
 ]
